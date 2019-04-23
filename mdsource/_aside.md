c79431861f51b17ef3d5850ff5037da868e26a04
songali <weiqiao525@sina.com>
2019-04-23 20:57:09 +0800

    样式调整this指向

--- mdsource/_aside.md
+++ mdsource/_aside.md
@@ -1,7 +1,10 @@
 
 - js基础
   - [this作用域](/pages/base/this.md)
+    - [关于this](/pages/base/this.md?#id=true)
     - [解释作用域](/pages/base/this.md?#id=true)
+    - [变量提升问题](/pages/base/this.md?#id=true)
+    - [闭包](/pages/base/this.md?#id=true)
     - [this指向情况](/pages/base/this.md?#id=true)
 
 - 性能
---
+++ static/src/index.js
@@ -0,0 +1,63 @@
+function getQueryString(urlSearch, name) { 
+  if (!urlSearch) { return null }
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+  var r = urlSearch.substr(1).match(reg); 
+  if (r != null) return r[2]; 
+  return null; 
+} 
+var page = {
+  init () {
+    console.log('start')
+    this.bindEvent()
+  },
+  bindEvent () {
+   
+    function createTitle () {
+      let str = ''
+      let url = window.location.href.split('#')[0]
+      // 去掉http的头和域名
+      let formatUrl = url.split('\/').slice(3).join('\/')
+      
+      const eles = $('h1, h2, h3, h4, h5, h6')
+      // const eles = $('h4')
+      console.log(eles.length)
+      for (let i = 0, len = eles.length; i < len; i++) {
+        str += '- [/' + eles[i].innerHTML + '](' + formatUrl + '?#id=true)' + '\n'
+      }
+      console.log(str)
+    }
+    $('#dealMarkDown').on('click', function (e) {
+      let target = e.target || e.srcElement
+      var operatstr = target.getAttribute('data-opera')
+      switch (operatstr) {
+        case 'createTitle':
+        createTitle()
+        break
+        default:
+      }
+    })
+
+    $('.main-aside').on('click', function (e) {
+      let target = e.target || e.srcElement
+      let url
+      if (target.tagName.toLowerCase() === 'a') {
+        url = target.getAttribute('href')
+        url = url ? decodeURIComponent(url) : null
+      } else {
+        return
+      }
+      let id = getQueryString(url.split('?')[1], 'id')
+      let titlele = $('#' + id)
+      let top = titlele.offset().top
+      document.documentElement.scrollTop = top 
+      document.body.scrollTop = top
+      $('.main-aside li').removeClass('active')
+      console.log($(this).parent())
+      $(target).parent().addClass('active')
+    })
+  }
+}
+
+$(document).ready(function () {
+  page.init()
+})
